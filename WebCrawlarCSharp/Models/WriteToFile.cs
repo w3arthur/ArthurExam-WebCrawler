b6d3d4af9c31a34eddf3661d96ad1c8b042ac602
w3arthur <w3arthur@gmail.com>
2022-11-14 18:40:16 +0200

    refactoring

--- WebCrawlarCSharp/Models/WriteToFile.cs
+++ WebCrawlarCSharp/Models/WriteToFile.cs
@@ -12,7 +12,6 @@ namespace WebCrawler.Models
     {
         public static async Task Run(List<Result> results, string filePath, string fileName)
         {
-            Console.WriteLine("Async WriteToFile Thread " + Thread.CurrentThread.ManagedThreadId);
             string jsonToFile = await Task.Run(() => JsonConvert.SerializeObject(results));
             string jsonToFile_results = "{" + "\"results\":" + jsonToFile + "}";
             await System.IO.File.WriteAllTextAsync(System.IO.Path.Combine(filePath, fileName), jsonToFile_results);
